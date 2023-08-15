a853cf9663f962af4057cda64eb76086f96826cd
aendirr <118621878+Aendirr@users.noreply.github.com>
2023-08-15 06:56:18 +0300

    Update discord_api.py

--- app/discord_bot/discord_api.py
+++ app/discord_bot/discord_api.py
@@ -17,12 +17,12 @@ class MyClient(discord.Client):
             return
         command, user_message=None, None
 
-        for text in ['/ai','/baggins','/shire','/cgpt']:
+        for text in ['/ai','/chatgpt']:
             if message.content.startswith(text):
                 command=message.content.split(' ') [0]
                 user_message=message.content.replace(text, '')
                 print(command, user_message)
-        if command == '/ai' or command == '/baggins' or command == '/shire' or command == '/cgpt':
+        if command == '/ai' or command == '/chatgpt':
             bot_response = chatgpt_response(prompt=user_message)
             await message.channel.send(f"Answer: {bot_response}")
 
